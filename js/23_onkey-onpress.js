3b2726b7afcec87246544e85d2ade19792f052b4
sieunn <73224848+sieunn@users.noreply.github.com>
2024-05-22 17:49:44 +0900

    유효성검사 정규식

--- js/23_onkey-onpress.js
+++ js/23_onkey-onpress.js
@@ -3,6 +3,10 @@ function 기능만들기(){
     기능.value = 기능.value.toUpperCase();
 }
 
-function 엔터기능(){
-    alert("입력한 내용이 전달되었습니다.");
+function 엔터기능(e){
+    //만약 엔터키를 눌럿을때
+    if(e.key === 'Enter'){
+        alert("입력한 내용이 전달되었습니다.");
+    }
+   
 }
